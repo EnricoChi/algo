68e4826873d54780215edd791d9b7df4375b8c7a
Muromati <enricoseo@gmail.com>
2018-02-13 13:55:32 +0300

    add set

--- 5/one.py
+++ 5/one.py
@@ -12,8 +12,9 @@
 # (при желании можно увеличить размер массива и диапазон чисел в 10/100 раз).
 # При замере производительности рекомендуется не печатать сообщения в консоль.
 
-from collections import Counter
+
 from random import randint
+from collections import Counter
 
 
 def search_lst(lst):
@@ -57,10 +58,25 @@ def counter_lst(lst):
 '''
 
 
-# test_lst = [randint(0, 1000000) for i in range(1000000)]
-# print(counter_lst(test_lst))
+def search_set(lst):
+    result_set = set()
+    set1 = set()
+    for _ in range(len(lst)):
+        current = lst.pop()
+        if current not in set1:
+            set1.add(current)
+        else:
+            result_set.add(current)
+    return result_set
 
-test_lst = [9, 4, 6, 4, 10, 0, 4, 0, 6, 8]
-# assert search_lst(test_lst) == {0, 4, 6}
-assert counter_lst(test_lst) == [4, 6, 0]
+
+# test_lst = [9, 4, 6, 4, 10, 0, 4, 0, 6, 8]
+test_lst = [65, 65, 44, 46, 50, 10, 57, 54, 80, 82, 30, 44, 23, 68, 28, 45, 32, 84, 53, 54, 49, 37, 66, 93, 83, 7, 96, 20, 83, 29, 44, 24, 63, 70, 78, 31, 20, 93, 42, 15, 37, 0, 29, 14, 75, 61, 95, 66, 1, 17, 30, 86, 35, 64, 88, 18, 3, 40, 55, 59, 5, 80, 39, 79, 70, 17, 33, 63, 24, 69, 12, 47, 84, 58, 100, 58, 19, 3, 41, 75, 73, 17, 32, 84, 95, 60, 77, 46, 31, 41, 27, 62, 28, 81, 34, 58, 100, 36, 80, 61]  #[randint(0, 100) for i in range(100)]
+# print(test_lst)
+# print(search_set(test_lst))
+
+
+assert search_set(test_lst) == {3, 17, 20, 24, 28, 29, 30, 31, 32, 37, 41, 44, 46, 54, 58, 61, 63, 65, 66, 70, 75, 80, 83, 84, 93, 95, 100}
+
+# assert counter_lst(test_lst) == [4, 6, 0]
 
